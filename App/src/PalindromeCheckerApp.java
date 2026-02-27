b16e8c2ed90fd889004cf53c7b2872c945e9702d
GautamVinay <gautamv2006@gmail.com>
2026-02-27 09:29:21 +0530

    WELCOME MESSAGE

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,20 @@
+
+public class PalindromeChecker {
+
+    // Application constants
+    static final String APP_NAME = "PalindromeChecker";
+    static final String APP_VERSION = "Version 1.0";
+
+
+    public static void main(String[] args) {
+
+        // Display welcome message and application info
+        System.out.println("=================================");
+        System.out.println("      Welcome to " + APP_NAME +" " + APP_VERSION);
+        System.out.println("=================================");
+        System.out.println("This application checks whether a word or phrase is a palindrome.");
+        System.out.println();
+
+        System.out.println("Application is ready...");
+    }
+}
